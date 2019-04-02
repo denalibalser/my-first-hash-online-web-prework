c741b56d82e066a4acbbcb3ed986b363346d6f29
Denali Balser <denali.balser167@gmail.com>
2019-04-02 18:34:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,7 +14,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-oil paintings 
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -23,14 +23,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["gun powder"] = 4
 
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  shipping_manifest
 end
