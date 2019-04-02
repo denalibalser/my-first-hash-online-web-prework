a348e3d74c491bb78ac4c2b83e8dfecee7dcbecc
Denali Balser <denali.balser167@gmail.com>
2019-04-02 18:37:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,8 +10,8 @@ end
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
+                  "oil paintings" => 3,
+                  "porcelain vases" => 2
                  }
 
 shipping_manifest["oil paintings"]
